6f8ea36b6c92c093c206abc513a036e807ed096e
prasanth <prasanth@gmail.com>
2024-12-13 08:09:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import Navbar from './components/Navbar' 
+import HeaderTwo from './components/HeaderTwo'
+import Products from './components/Products'
+import Footer from './components/Footer'
+
+const App = () =>(
+  <>
+    <Navbar />
+    <HeaderTwo />
+    <Products />
+    <Footer />
+  </>
+)
+export default App
